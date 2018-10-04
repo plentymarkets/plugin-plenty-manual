b8a51ddaf23b2d179f891d596ec645948b142d49
BuildMonkey <buildmonkey@plentymarkets.com>
2018-10-04 11:05:46 +0200

    ADD auto-generated contents

--- src/Helpers/Metadata.php
+++ src/Helpers/Metadata.php
@@ -5619,7 +5619,7 @@ class Metadata {
 						"url" => "/en/basics/automation/installing-plentybase",
 						"level" => 3,
 						"title" => "Installing plentyBase",
-						"keywords" => "plentyBase, processes, printer, printing, Installing printer, run processes, execute processes, automation, print history",
+						"keywords" => "plentyBase, processes, printer, printing, Installing printer, run processes, execute processes, automation, print history, plentymarkets Client, Base, base",
 						"description" => "Installing plentyBase",
 						"navAlias" => "",
 						"linkListInto" => "Welcome to the topic Installing plentyBase. The following information is available:",
