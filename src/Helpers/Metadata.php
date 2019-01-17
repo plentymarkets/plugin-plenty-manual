ad21805c93ea372fd609e472a3cf3e416f68c672
BuildMonkey <buildmonkey@plentymarkets.com>
2019-01-17 15:36:33 +0100

    ADD auto-generated contents

--- src/Helpers/Metadata.php
+++ src/Helpers/Metadata.php
@@ -8435,6 +8435,21 @@ class Metadata {
 						"linkListInto" => "Welcome to the topic Best Practices: UPS. The following information is available:",
 						"children" => [],
 						"hasChildren" => false
+					],
+					[
+						"id" => "0e97981645222ce5d8e178b77003bf4b",
+						"path" => "order-processing/fulfilment/best-practice-postcode-filter",
+						"position" => 70,
+						"urlName" => "best-practice-postcode-filter",
+						"url" => "/en/order-processing/fulfilment/best-practice-postcode-filter",
+						"level" => 3,
+						"title" => "Best Practices: Postcode Filter",
+						"keywords" => "postcode, postcodes, poastal code, postal code, filter, event procedures",
+						"description" => "",
+						"navAlias" => "",
+						"linkListInto" => "Welcome to the topic Best Practices: Postcode Filter. The following information is available:",
+						"children" => [],
+						"hasChildren" => false
 					]
 				],
 				"hasChildren" => true
