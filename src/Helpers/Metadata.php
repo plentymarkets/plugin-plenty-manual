4b29fc474daed5b95410291034aee892be75d773
BuildMonkey <buildmonkey@plentymarkets.com>
2019-06-03 08:37:06 +0200

    ADD auto-generated contents

--- src/Helpers/Metadata.php
+++ src/Helpers/Metadata.php
@@ -4859,7 +4859,7 @@ class Metadata {
 				"level" => 2,
 				"title" => "Cdiscount",
 				"keywords" => "Cdiscount, Marktplatz",
-				"description" => "Multi-Channel in plentymarkets: Richten Sie die Schnittstelle zum Marktplatz Cdiscount in Ihrem plentymarkets System ein.",
+				"description" => "Multi-Channel in plentymarkets: Richte die Schnittstelle zum Marktplatz Cdiscount in deinem plentymarkets System ein.",
 				"navAlias" => "",
 				"linkListInto" => "Willkommen im Themenbereich Cdiscount. Hier finden Sie folgende Informationen:",
 				"children" => [],
