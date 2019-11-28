7b7a902840843554f306a15607087a0251ae31a8
BuildMonkey <10546500+plentymonkey@users.noreply.github.com>
2019-11-28 14:16:27 +0100

    ADD auto-generated contents

--- src/Helpers/Metadata.php
+++ src/Helpers/Metadata.php
@@ -9613,22 +9613,6 @@ class Metadata {
 				"hasChildren" => false
 			],
 			[
-				"id" => "65b437f5773b55e5ea8e2e1216b66c35",
-				"path" => "data/data-cleanup",
-				"position" => 50,
-				"urlName" => "data-cleanup",
-				"url" => "/en/data/data-cleanup",
-				"level" => 2,
-				"title" => "Carrying out data cleanup",
-				"keywords" => "Cleanup, cleansing, data cleansing, data purge, automatic deletion, deleting addresses, deleting unused addresses, deleting unused contacts, deleting orders, deleting history, deleting IP addresses",
-				"description" => "Learn how to clean up data automatically in plentymarkets.",
-				"navAlias" => "",
-				"linkListInto" => "Welcome to the topic Carrying out data cleanup. The following information is available:",
-				"languageID" => "BPZ0VS4",
-				"children" => [],
-				"hasChildren" => false
-			],
-			[
 				"id" => "fd12be59d194b8ad1b90a14b6e4d2649",
 				"path" => "data/data-cleansing",
 				"position" => 50,
