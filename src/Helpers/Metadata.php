52e88d3333ed8c3605e1d0dd2efff6193b9a74ec
BuildMonkey <10546500+plentymonkey@users.noreply.github.com>
2021-04-09 10:11:35 +0200

    ADD auto-generated contents

--- src/Helpers/Metadata.php
+++ src/Helpers/Metadata.php
@@ -22517,7 +22517,7 @@ class Metadata {
 		"title" => "plentymarkets Changelog",
 		"keywords" => "",
 		"description" => "",
-		"navAlias" => "Changelog 31. März 2021",
+		"navAlias" => "Changelog 07. März 2021",
 		"linkListInto" => "Willkommen im Themenbereich plentymarkets Changelog. Hier findest du folgende Informationen:",
 		"languageID" => "",
 		"children" => 		[
